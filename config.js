0e6f1607ec55a429fd46ba17ab1301cc2f044e4e
Lucasrx08 <lucasrigaux8@gmail.com>
2026-09-07 16:08:26 +0200

    V2: paramètres application et Instagram

--- config.js
+++ config.js
@@ -2,5 +2,7 @@ window.APP_CONFIG = {
   appName: "Bon Sauveur Sport",
   supabaseUrl: "",
   supabaseAnonKey: "",
-  demoMode: true
+  demoMode: true,
+  instagramUrl: "",
+  appreciationsDeadline: "2026-11-30"
 };
