d006c4ae43f56b743da3c60df8f262cb4c989696
Lucasrx08 <lucasrigaux8@gmail.com>
2026-09-09 10:51:10 +0200

    V20.6: connect production app to Supabase

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 window.APP_CONFIG = {
   appName: "Association Sportive du Bon Sauveur",
-  supabaseUrl: "",
-  supabaseAnonKey: "",
-  demoMode: true,
+  supabaseUrl: "https://qafwwsxmhiesbfaydcih.supabase.co",
+  supabaseAnonKey: "sb_publishable_BGl5FFyuBCLYFiS2zBjRLQ_ml4T86VN",
+  demoMode: false,
   productionMode: true,
   enableExternalGrammar: false,
   instagramUrl: "https://www.instagram.com/as_bs50/",
