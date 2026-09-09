19a4096fc362053eabda5e5dec3422b1cfe1e470
Lucasrx08 <lucasrigaux8@gmail.com>
2026-09-09 07:51:25 +0200

    V20: switch to production-safe configuration

--- config.js
+++ config.js
@@ -2,7 +2,9 @@ window.APP_CONFIG = {
   appName: "Association Sportive du Bon Sauveur",
   supabaseUrl: "",
   supabaseAnonKey: "",
-  demoMode: true,
+  demoMode: false,
+  productionMode: true,
+  enableExternalGrammar: false,
   instagramUrl: "https://www.instagram.com/as_bs50/",
   instagramFeedEndpoint: "",
   languageToolUrl: "https://api.languagetool.org/v2/check"
