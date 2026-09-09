0314c74e04e8c980fe8f45daebcbbbc18c7cf7f2
Lucasrx08 <lucasrigaux8@gmail.com>
2026-09-09 09:54:17 +0200

    V20.1: refresh PWA cache for export layouts

--- sw.js
+++ sw.js
@@ -1,8 +1,7 @@
-const CACHE='as-bon-sauveur-v20-20260909-3';
+const CACHE='as-bon-sauveur-v20-20260909-4';
 const CORE=[
   './','./index.html','./config.js','./manifest.webmanifest',
   './v19.css','./v20.css','./v19-app.js','./v20-exports.js','./v20-preflight.js','./v20-bridge.js','./v20-reconcile.js','./v20-admin.js','./v20-postboot.js',
-  './assets/convocation-template.png.b64','./assets/programme-template.png.b64',
   './assets/logo-as.png','./assets/logo-ecoledirecte.svg','./assets/shop-tshirt-skyblue.webp','./assets/shop-sweat-sapphire.webp'
 ];
 self.addEventListener('install',event=>{
