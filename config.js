ba498504da9ad44427a47b4476edbe6e69133490
Lucasrx08 <lucasrigaux8@gmail.com>
2026-09-15 13:15:42 +0200

    V22.1: disable automatic Instagram feed loading

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ window.APP_CONFIG = {
   productionMode: true,
   enableExternalGrammar: false,
   instagramUrl: "https://www.instagram.com/as_bs50/",
-  instagramFeedEndpoint: "https://qafwwsxmhiesbfaydcih.supabase.co/functions/v1/instagram-feed",
+  instagramFeedEndpoint: "",
   languageToolUrl: "https://api.languagetool.org/v2/check"
 };
