0bf4f417f9c16359e0afd94020c3e217fd896c63
Lucasrx08 <lucasrigaux8@gmail.com>
2026-09-17 12:26:02 +0200

    V30.0.4: publish release metadata

--- v30-release.js
+++ v30-release.js
@@ -1,5 +1,5 @@
 (() => {
 'use strict';
-window.__BS_RELEASE={major:30,label:'V30',version:'30.0.0',channel:'stable'};
+window.__BS_RELEASE={major:30,label:'V30.0.4',version:'30.0.4',channel:'stable'};
 document.documentElement.dataset.appVersion='30';
 })();
