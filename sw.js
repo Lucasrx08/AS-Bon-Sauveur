0bfb92b3da1dac381e3c447169b108d3f827541b
Lucasrx08 <lucasrigaux8@gmail.com>
2026-09-25 09:23:37 +0200

    Force le renouvellement du cache PWA en v31.6.1

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const APP_VERSION='31.6.0';
-const BUILD_ID='20260925-table-sorting';
+const APP_VERSION='31.6.1';
+const BUILD_ID='20260925-table-sorting-hotfix';
 const CACHE_PREFIX='as-bon-sauveur-build-';
 const CACHE_NAME=`${CACHE_PREFIX}${APP_VERSION}-${BUILD_ID}`;
 const META_KEY='./__bs_build_meta__';
@@ -7,10 +7,11 @@ const OFFLINE_URL='./offline.html';
 
 const PRECACHE=[
   OFFLINE_URL,
-  './manifest.webmanifest?v=31.6.0',
-  './assets/logo-as.png?v=31.6.0',
-  './v31-mobile.css?v=31.6.0',
-  './v31-mobile-lock.js?v=31.6.0'
+  './manifest.webmanifest?v=31.6.1',
+  './assets/logo-as.png?v=31.6.1',
+  './v31-mobile.css?v=31.6.1',
+  './v31-mobile-lock.js?v=31.6.1',
+  './v31-6-table-sort.js?v=31.6.1'
 ];
 
 self.addEventListener('install',event=>{
